67a22c5271290a298efcb96baf1c3d60317aea61
Max Veit <55698339+maxveit@users.noreply.github.com>
2020-12-02 11:44:56 +0100

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-env_container_registry=quay-quay-quay.mycluster-us-sout-363772-a01ee4194ed985a1e32b1d96fd4ae346-0000.us-south.containers.appdomain.cloud/stocktrader/
-env_cluster=https://c114-e.us-south.containers.cloud.ibm.com:31476
+env_container_registry="quay-quay-quay.mycluster-us-sout-363772-a01ee4194ed985a1e32b1d96fd4ae346-0000.us-south.containers.appdomain.cloud/stocktrader/"
+env_cluster="https://c114-e.us-south.containers.cloud.ibm.com:31476"
